e9c26446e0a8393e8120f06d7210e0bcc36da16d
ibrahim <isenkaya03@gmail.com>
2025-04-05 00:29:11 +0300

    Dummy Level datas

---
+++ Assets/Scripts/Data/LevelData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelData", menuName = "ScriptableObjects/LevelData", order = 1)]
+public class LevelData : ScriptableObject
+{
+    public int ColumnCount;
+    public int RowCount;
+        
+    public int targetMoves;
+    public int targetScore;
+
+}
+ 
